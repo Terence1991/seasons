1c2ab6530fe87216720c76bf5024c641b871494d
Terence1991 <cplterencehawes@gmail.com>
2019-01-04 21:08:36 +0000

    refactored

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-import React from 'react'
+import React, {} from 'react'
 import ReactDOM from 'react-dom'
 
-const App = () => {
-  window.navigator.geolocation.getCurrentPosition(
-    (position) => console.log(position), 
-    (err) => console.log(err)
-  )
-  return (
-    <div> hi there</div>
-  )
+class App extends React.Component {
+  render() {
+    window.navigator.geolocation.getCurrentPosition(
+         (position) => console.log(position), 
+         (err) => console.log(err)
+       );
+      return <div>Lattitude: </div>
+    }
 }
 
 ReactDOM.render(
