914d3f4e8efb05a3423d6e317a5126a6e789d16d
Terence1991 <cplterencehawes@gmail.com>
2019-01-08 21:01:35 +0000

    season implemented

--- src/Season.js
+++ src/Season.js
@@ -1,7 +1,18 @@
 import React from 'react'
 
+
+const getSeason = (lat, month) => {
+  if(month > 2 && month < 9) {
+     return lat > 0 ? 'summer' : 'winter'
+  } else {
+    return lat > 0 ?  'winter' : 'summer'
+  }
+}
+
 const Season = (props) => {
-  console.log("this is my props", props.lat)
+ const season = getSeason(props.lat, new Date().getMonth()) 
+  
+ console.log(season)
   return (
 <div> seasons greeting</div>
   )
