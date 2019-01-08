03044c7926fad7c9b6012e4647e62a8bd4d3a78b
Terence1991 <cplterencehawes@gmail.com>
2019-01-08 18:50:46 +0000

    error message rendered

--- src/index.js
+++ src/index.js
@@ -5,19 +5,27 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     
-    this.state = {lat: null}
+    this.state = {lat: null, errorMessage: ''}
     window.navigator.geolocation.getCurrentPosition(
       (position) => {
         console.log("fuck", position)
         this.setState({lat: position.coords.latitude})
       }, 
-      (err) => console.log(err)
+      (err) => {
+        this.setState({errorMessage: err.message})
+      }
     );
   }
   
   
   render() {
-      return <div>Lattitude:{this.state.lat}</div>
+      return (
+    <div>
+      Lattitude:{this.state.lat}
+      <br />
+      Error: {this.state.errorMessage} 
+    </div>
+      )  
     }
 }
 
