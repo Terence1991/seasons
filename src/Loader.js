b1eb8611bd40d163df80a442c1b434cb6272f57b
Terence1991 <cplterencehawes@gmail.com>
2019-01-10 19:38:06 +0000

    loader fixed

--- src/Loader.js
+++ src/Loader.js
@@ -5,12 +5,10 @@ import React from 'react'
 
 const Loader = () => {
   return (
-    <div class="ui segment">
-  <p></p>
   <div class="ui active dimmer">
     <div class="ui loader"></div>
   </div>
-</div>
+
   )
 }
 
